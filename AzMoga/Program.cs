0da06b23dbd31c5bab4059c061a95b1961bb951b
Jordan-Dimitrov <dani27game@gmail.com>
2023-06-03 09:08:45 +0300

    base

---
+++ AzMoga/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzMoga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
